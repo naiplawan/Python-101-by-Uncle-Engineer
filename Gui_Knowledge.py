7e792d212ad9c06882e584544b8dd7ff4b0d937d
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-02-28 18:29:51 +0700

    Add files via upload

---
+++ Gui_Knowledge.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+#GUI Design
+GUI = Tk()
+GUI.title('โปรแกรมบันทึกข้อมูล')
+GUI.geometry('500x300')
+L1 = Label(GUI,text='โปรแกรมบันทึกความรู้', font=('Angsana New', 30), fg='green')
+L1.place(x=30,y=50)
+
+#FrameDesign
+FB1 = Frame(GUI)  
+FB1.place(x=150,y= 50)
+
+################
+def Button1 () :
+    pass
+def Button2 () :
+    text = 'ตอนนี้มีเงินในบัญชีอยู่ 300 บาท'
+    messagebox.showinfo('เงินในบัญชี' ,text)
+
+#Button Design
+B1 = ttk.Button(FB1,text ='กดเพื่อสั่งสินค้า',command = Button1)
+B1.pack(ipadx=20,ipady=20)
+B2 = ttk.Button(FB1,text ='เงินมีอยู่กี่บาท',command=Button2)
+B2.pack(ipadx=20,ipady=20)
+L1.pack() 
+################
+
+
+GUI.mainloop()
